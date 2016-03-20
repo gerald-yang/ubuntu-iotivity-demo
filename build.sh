951c480dc378e3c4d6c5b4dc93262e747d88b8fe
Gerald Yang <gerald.yang@canonical.com>
2016-03-20 21:53:38 +0800

    Add arm to build script.

--- build.sh
+++ build.sh
@@ -1,29 +1,62 @@
+if [ -z "$IOTIVITY_ROOT" ]; then
+	IOTIVITY="iotivity-1.0.1"
+fi
+
 print_usage() {
 	echo "Usage:"
-	echo "build.sh [TARGET]"
+	echo "build.sh [TARGET] [ARCH]"
 	echo " "
 	echo "Without TARGET:"
-       	echo "build whole iotivity project and ubuntu-demo"
+       	echo "build whole iotivity project and ubuntu-demo for x86_64"
 	echo " "
 	echo "TARGET:"
 	echo "demo          build ubuntu-demo folder only (need to build the whole iotivity project first)"
 	echo "demo-snappy   copy binaries and libraries to snappy folder and build snap package"	
 	echo "              (need to build demo first)"
 	echo " "
+	echo "ARCH:"
+	echo "x86_64"
+	echo "armeabi-v7a-hard"
+	echo " "
 }
 
-
-if [ -z $1 ]; then
-	cd iotivity-1.0.1 && ln -f -s ../ubuntu-demo ubuntu-demo && scons -j4
-elif [ "$1" == "demo" ]; then
-	cd iotivity-1.0.1 && ln -f -s ../ubuntu-demo ubuntu-demo && scons -j4 ubuntu-demo
-elif [ "$1" == "demo-snappy" ]; then
-	cp -f iotivity-1.0.1/out/linux/x86_64/release/*.so snappy/iotivity-nucdemo/lib/
-	cp -f iotivity-1.0.1/out/linux/x86_64/release/ubuntu-demo/nucdemo snappy/iotivity-nucdemo/
-	cd snappy/iotivity-nucdemo && snapcraft clean && snapcraft
+if [ -z "$1" ]; then
+	TARGET="all"
+elif [ "$1" == "demo" ] || [ "$1" == "demo-snappy" ]; then
+	TARGET=$1
 elif [ "$1" == "-h" ]; then
 	print_usage
+	exit 0
 else
-	echo "Unknow command $1"
+	echo "Unknown target $2"
 	print_usage
+	exit 1
+fi
+
+if [ -z "$2" ] || [ "$2" == "x86_64" ] ; then
+	ARCH="x86_64"
+elif [ "$2" == "armeabi-v7a-hard" ]; then
+	ARCH=$2
+else
+	echo "Unknown architecture $2"
+	print_usage
+	exit 1
+fi
+
+if [ -z "$3" ]; then
+	CORE=1
+else
+	CORE=$3
+fi
+
+
+
+if [ "$TARGET" == "all" ]; then
+	cd $IOTIVITY && ln -f -s ../ubuntu-demo ubuntu-demo && scons -j$CORE TARGET_ARCH=$ARCH TARGET_OS=linux
+elif [ "$1" == "demo" ]; then
+	cd $IOTIVITY && ln -f -s ../ubuntu-demo ubuntu-demo && scons -j$CORE TARGET_ARCH=$ARCH TARGET_OS=linux ubuntu-demo
+elif [ "$1" == "demo-snappy" ]; then
+	cp -f $IOTIVITY/out/linux/$ARCH/release/*.so snappy/iotivity-nucdemo/lib/
+	cp -f $IOTIVITY/out/linux/$ARCH/release/ubuntu-demo/nucdemo snappy/iotivity-nucdemo/
+	cd snappy/iotivity-nucdemo && snapcraft clean && snapcraft
 fi
